4a5415265430e7eb93f882130b6889403b6537eb
rahmatullah <skrahmathgousi@400.com>
2025-07-29 17:53:08 +0530

    Updated

--- src/main/java/com/redis/schedulingconfig/RedisAutoCleanup.java
+++ src/main/java/com/redis/schedulingconfig/RedisAutoCleanup.java
@@ -1,6 +1,5 @@
 package com.redis.schedulingconfig;
 
-
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -10,25 +9,28 @@ import org.springframework.stereotype.Component;
 @Component
 public class RedisAutoCleanup {
 
-    @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
+	@Autowired
+	private RedisTemplate<String, Object> redisTemplate;
 
-    @Scheduled(fixedRate = 60000) // every 60 seconds
-    public void cleanupExpiredRoleScreenData() {
-        Set<String> dataKeys = redisTemplate.keys("roleandscreen::*");
+	@Scheduled(fixedRate = 60000)
+	public void cleanupExpiredRoleScreenData() {
+		Set<String> dataKeys = redisTemplate.keys("roleandscreen::*");
+		if (dataKeys == null || dataKeys.isEmpty())
+			return;
 
-        if (dataKeys == null || dataKeys.isEmpty()) return;
+		for (String dataKey : dataKeys) {
+			String roleName = dataKey.replace("roleandscreen::", "");
+			String accessKey = "access::roleandscreen::" + roleName;
 
-        for (String dataKey : dataKeys) {
-            String roleName = dataKey.replace("roleandscreen::", "");
-            String accessKey = "access::roleandscreen::" + roleName;
+			Boolean accessExists = redisTemplate.hasKey(accessKey);
 
-            Boolean accessExists = redisTemplate.hasKey(accessKey);
+			if ((accessExists == null || !accessExists)
+					&& !(roleName.equals("ZONAL") || roleName.equals("TL") || roleName.equals("FULLSTACK"))) {
 
-            if (accessExists == null || !accessExists) {
-                redisTemplate.delete(dataKey);
-                System.out.println("🧹 Deleted stale role data for: " + dataKey + " (no access key)");
-            }
-        }
-    }
+				redisTemplate.delete(dataKey);
+				System.out.println("🧹 Deleted stale role data for: " + dataKey + " (no access key)");
+			}
+		}
+	}
+
 }
